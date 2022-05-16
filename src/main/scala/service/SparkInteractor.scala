d6299af2ff6c4ad5399f799bfce02a5a8aeec3c6
rajesh nellipudi <sudheerkumar.nellipudi@gmail.com>
2022-05-16 14:22:35 +0530

    completed phase one showing min max avg values for humidity sensors

---
+++ src/main/scala/service/SparkInteractor.scala
@@ -0,0 +1,38 @@
+package service
+
+import org.apache.spark.sql.SparkSession
+import service.SparkMatInteractor.{insertMock, readMock}
+import util.BaseDriver.{Fetch, Forecast, Insert}
+
+trait SparkInteractor {
+  def insertMock(passes: Int)(implicit s: SparkSession)
+  def insertSample(implicit s: SparkSession)
+  def readMock(implicit s: SparkSession)
+  def readSample(implicit s: SparkSession)
+}
+
+object SparkMatInteractor extends SparkInteractor {
+  override def insertMock(passes: Int)(implicit session: SparkSession): Unit = passes match {
+    case x if x > 0 => insertMock(passes-1)
+    case _ =>
+      val columns = Seq("sensor-id","humidity")
+      val sensors = (1 until (10000 + scala.util.Random.nextInt(10000))).map {
+        id => "s"+id.toString
+      }.toList
+      val sampleResult: List[(String,String)] = sensors.map(entry => {
+        if(entry.split("s")(1).toInt % 100 == 0) (entry,"")
+        else (entry, scala.util.Random.nextInt(100)+"")
+      }).toList
+      import session.implicits._
+      sampleResult.toDF(columns:_*).write.option("header",true).mode("append").csv("./spark-warehouse/temps/collections")
+  }
+
+  override def insertSample(implicit s: SparkSession): Unit = ???
+
+  override def readMock(implicit spark: SparkSession): Unit = {
+    val df = spark.read.option("header","true").option("mode", "PERMISSIVE") .csv("./spark-warehouse/temps/collections")
+    df.groupBy("sensor-id").agg("humidity" -> "min", "humidity" -> "avg", "humidity" -> "max").show
+  }
+
+  override def readSample(implicit s: SparkSession): Unit = ???
+}
--- src/main/scala/util/BaseDriver.scala
+++ src/main/scala/util/BaseDriver.scala
@@ -2,44 +2,78 @@ package util
 
 import java.io.File
 
+import akka.pattern.ask
+import akka.util.Timeout
 import com.typesafe.config.{Config, ConfigFactory}
 import config.AppConfig
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 import org.joda.time.DateTime
+import service.SparkMatInteractor
 import zio._
-import zio.{console, App , ZEnv, ZIO}
+import zio.{App, ZEnv, ZIO, console}
 
-import scala.collection.JavaConversions._
+import scala.annotation.tailrec
+import scala.concurrent.Future
+import scala.concurrent.duration.DurationInt
 
 object BaseDriver extends zio.App with Logging{
 	val name: String = this.getClass.getName
+	case class Forecast(sensorId: String, humidity: Int)
+	case class Insert(implicit session: SparkSession)
+	case class Fetch(implicit session: SparkSession)
+	implicit val timeout = Timeout(3.seconds)
+	import scala.concurrent.ExecutionContext.Implicits._
+	import scala.collection.JavaConversions._
 
-	def run(args: List[String]): URIO[console.Console, ExitCode] = {
-		val (sparkConf, config) = loadConfig(args.headOption)
+	def run(commandLineArgs: List[String]): URIO[console.Console, ExitCode] = {
+		logger info "try giving hadoop.home.dir system property / environment variable / path with winutils.exe folder path"
+		System.setProperty("hadoop.home.dir", "C:\\JAVA\\Hadoop")
+		val (sparkConf, config) = loadConfig(commandLineArgs.drop(1).headOption)
 		lazy val spark = SparkSession.builder()
 			.appName(name)
 			.master(config.master)
 			.config(sparkConf)
 			.getOrCreate()
-
 		val startTime = DateTime.now()
 		logger.info("Started at " + startTime.toString)
 
 		try{
-			run(spark, config)
+			run(spark, config, commandLineArgs)
 		} catch {
 			case e: Exception =>
 				logger.error("ERROR", e)
 				URIO(ExitCode.failure)
+
 		} finally {
 			val endTime = DateTime.now()
 			logger.info("Finished at " + endTime.toString() + ", took " + ((endTime.getMillis.toDouble - startTime.getMillis.toDouble) / 1000))
-			spark.stop()
+			logger info "cleaning up everything"
+			spark.stop
 		}
 	}
 
-	def run(session: SparkSession, config: AppConfig) = console.putStrLn("Welcome").exitCode
+	def run(session: SparkSession, config: AppConfig, params: List[String]) = {
+		implicit val sparksession = session
+		params.headOption match {
+			case Some(str) =>
+				val data = Forecast("s1",30)
+				val sample = (data.sensorId, data.humidity)
+				val sampleResult = Seq(sample, sample)
+				val columns = Seq("sensor-id","humidity")
+				import session.implicits._
+				sampleResult.toDF(columns:_*).write.option("header",true).csv("./spark-warehouse/temps/collections")
+			case None =>
+				callInsert(5+scala.util.Random.nextInt(3))
+				@tailrec
+				def callInsert(countDown: Int):Unit = countDown match {
+					case x if x > 0 => callInsert(countDown-1)
+					case _ => SparkMatInteractor.insertMock(countDown)
+				}
+				SparkMatInteractor.readMock
+		}
+		console.putStrLn("Welcome").exitCode
+	}
 
 	private def loadConfig(pathArg: Option[String]): (SparkConf, AppConfig) = {
 		val config: Config = pathArg.map { path =>
@@ -47,11 +81,10 @@ object BaseDriver extends zio.App with Logging{
 			if(!file.exists()){
 				throw new Exception("Config path " + file.getAbsolutePath + " doesn't exist!")
 			}
-
 			logger.info(s"Loading properties from " + file.getAbsolutePath)
 			ConfigFactory.load(ConfigFactory.parseFile(file).resolve())
 		}.getOrElse {
-			ConfigFactory.load()
+			ConfigFactory.load(ConfigFactory.parseFile(new File("\\src\\main\\resources\\reference.conf")).resolve())
 		}
 
 		val sparkValues = config.entrySet().collect {
